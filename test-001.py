6f16ef67b25d0f013a2c04e1fe14ad0b5331c6ca
Alex <chekhniy@ukr.net>
2021-11-14 22:44:21 +0200

    start

---
+++ test-001.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+baseURL = 'https://www.gsmarena.com/makers.php3'
+
+urlParts = urlparse(baseURL)
+hostname = urlParts.hostname
+scheme = urlParts.scheme
+
+response = requests.get(baseURL)
+soup = BeautifulSoup(response.text, 'lxml')
+
+brandsTable = soup.find_all('div', class_='main main-makers l-box col float-right')
+
+brandLinks = {}
+
+# decomposition to brands
+for tag in brandsTable:
+    brandList = tag.find_all('a')
+    for brand in brandList:
+        brandLinks[brand.text[:len(brand.text) - len(brand.span.text)]] = scheme + '://' + hostname+'/'+brand.get('href')
+
+# decomposition to pages
+
+pagesToParse = []
+for brand in brandLinks:
+    link = brandLinks[brand]
+    brandPageResponse = requests.get(link)
+    brandPageSoup = BeautifulSoup(brandPageResponse.text, 'lxml')
+    brandNav = brandPageSoup.find_all('div', class_='nav-pages')
+
+    brandPageLinks = [link, ]
+
+    for page in brandNav:
+        pgLinks = page.find_all('a')
+        for pgLinkValue in pgLinks:
+            brandPageLinks.append(scheme + '://' + hostname+'/'+pgLinkValue['href'])
+
+    pagesToParse.append({'brand': brand, 'pages': brandPageLinks})
+
+print(len(pagesToParse))
